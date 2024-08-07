90cee94a9bfca21ae59097b39caa7c98f919a4ee
Pranom788 <cageharper49@gmail.com>
2024-08-07 05:06:14 +0000

    let, ver and const

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 1
+let accountEmail = "pranom123@gmail.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+
+// accountId = 2 // will not change as it is constant 
+
+console.log(accountId);
+
+accountEmail = "qw@gamil.com"
+accountPassword = "54321"
+accountCity = "Bihar"
+console.table([accountId,accountEmail,accountPassword,accountCity])
